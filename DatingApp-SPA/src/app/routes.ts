63040b35ff073d5715c5dc8d6cbf3dc9900c2127
Roofy <gmichal1995@interia.pl>
2020-02-09 18:47:26 +0100

    End of Section 8

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router'
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
