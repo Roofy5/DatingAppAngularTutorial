d22f2ec585f805ed6255c7e93b75fd5a8e65c420
Roofy <gmichal1995@interia.pl>
2020-02-17 17:00:01 +0100

    End of Section 10

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API.Helpers
                     opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
 
             CreateMap<Photo, PhotosForDetaildeDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
